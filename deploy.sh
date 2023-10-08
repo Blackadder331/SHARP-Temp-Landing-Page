f0a8eed7c083e9667c501b59801d0dd3de237b27
R Sharp <reynolds.sharp@gmail.com>
2023-10-07 20:13:50 -0500

    Deploy script added

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# pull new repo from git
+git pull
+
+# find the existing docker CONTAINER ID (IMAGE is temp-landing)
+CONTAINER_ID=`docker ps | awk '/temp-landing/ {print $1}'`
+
+# build and start the new one
+docker build --tag temp-landing .
+docker stop $CONTAINER_ID
+docker rm $CONTAINER_ID
+docker run --detach --network proxy-compose_default --name=temp-landing --restart=always temp-landing
+docker images prune
+
+echo "temp-landing is deployed"
